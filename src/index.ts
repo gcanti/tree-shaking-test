917e3a58b8da9ac54e9091b02664494ed456cf9b
gcanti <giulio.canti@gmail.com>
2024-05-01 13:28:37 +0200

    z.string().safeParse('')

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import * as Either from 'effect/Either';
-import * as Arr from 'effect/Array';
+import { z } from 'zod'
 
-const divide = (a: number, b: number) => (b === 0 ? Either.left('cannot divide by zero') : Either.right(a / b));
-const input = [2, 3, 5];
-const program = Arr.head(input).pipe(Either.fromOption(() => 'empty array'), Either.flatMap((b) => divide(10, b)), Either.match({
-    onLeft: (e) => `Error: ${e}`,
-    onRight: (a) => `Result: ${a}`
-}));
-console.log(program);
+z.string().safeParse('')
